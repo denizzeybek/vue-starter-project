a640c45fa3e1061896f60e1a66ed3d0789a2160a
denizzeybek <zeybekdeniz64@gmail.com>
2024-06-09 03:28:55 +0300

    set tailwind darkmode from auto to selector

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,7 @@ const { colors } = require('./src/constants/colors')
 /** @type {import('tailwindcss').Config} */
 
 export default {
+  darkMode: 'selector',
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
     extend: {
